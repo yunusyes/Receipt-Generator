b2362ce8fb0a55f5048f72762dc4f803660f7078
Yunus Yesil <yunusyesil333@gmail.com>
2024-10-04 17:15:16 +0200

    added receipt.py

---
+++ receipt.py
@@ -0,0 +1,25 @@
+class Shopping_cart: 
+
+    def receipt(self):
+        self.shopping_cart = []
+        self.amount = 0
+        self.item = ""
+        
+        while self.item != "q":
+            self.item = input("Enter a food to buy (q to quit): ")
+            if self.item == "q":
+                print("----- YOUR CART -----")
+                for i in self.shopping_cart:
+                    print(i)
+                print(f"Your total is: ${self.amount}")
+                break  
+            else:
+                self.shopping_cart.append(self.item) 
+                self.price = int(input(f"Enter the price of a {self.item}: $"))
+                self.amount += self.price
+                 
+def main():
+    sc = Shopping_cart()
+    sc.receipt()
+    
+main()
